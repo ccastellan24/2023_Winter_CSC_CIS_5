23214ab2e542b73cfcd3cebfe9211c0520c62b87
ccastellan24 <111922586+ccastellan24@users.noreply.github.com>
2023-01-07 21:18:36 -0800

    Add files via upload

---
+++ Homework.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Declare Variables
+    int payamt = 2200;
+    int payprd = 26;
+    int annpay = payamt * payprd;
+    
+    //Display annual amount made
+    cout<< "Total Annual Pay = $" << annpay<< endl;
+    
+    
+    
+    
+    return 0;
+}
